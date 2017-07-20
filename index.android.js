e0c3fba9d42754f80d8fc13afe95c77e768f33fe
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-07-19 23:29:04 -0500

    (feat) Implement basic Auth0 log-in

--- index.android.js
+++ index.android.js
@@ -1,1 +1,13 @@
 import './App';
+import Auth0 from 'react-native-auth0';
+const auth0 = new Auth0({ domain: 'kmssquared.auth0.com', clientId: 'OZC5JTbnOI1eQJ6-XF0rFFMZqgskh6g6' });
+
+auth0
+.webAuth
+.authorize({scope: 'openid email', audience: 'https://kmssquared.auth0.com/userinfo'})
+.then(credentials =>
+  console.log(credentials)
+  // Successfully authenticated
+  // Store the accessToken
+)
+.catch(error => console.log(error));
