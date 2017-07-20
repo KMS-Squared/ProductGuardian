ca2638f074ce8a55f391e0fac384e8c67d798e0b
Mark <suyatdesign@gmail.com>
2017-07-19 19:42:14 -0700

    implemented edit profile component

---
+++ android/components/Profile.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import t from 'tcomb-form-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+let Form = t.form.Form;
+let User = t.struct({
+  username: t.String,
+  first_name: t.String,
+  last_name: t.String,
+  avoidables: t.String,
+  password: t.String
+});
+
+export default class Profile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.options = {}; // optional rendering options (see documentation)
+    // here we are: define your domain model
+    this.state = {
+      User: User
+    }
+  }
+
+  onPress () {
+    // call getValue() to get the values of the form
+    var value = this.refs.form.getValue();
+    if (value) { // if validation fails, value will be null
+      console.log(value);
+
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        {/* display */}
+        <Form
+          ref="form"
+          type={User}
+          options={this.options}
+        />
+        <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
