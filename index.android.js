fa6140753c157687670e4b14a7f39b439f94210f
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-07-19 23:56:40 -0500

    "Remove Auth0 credentials from index"

--- index.android.js
+++ index.android.js
@@ -1,13 +1,14 @@
 import './App';
 import Auth0 from 'react-native-auth0';
-const auth0 = new Auth0({ domain: 'kmssquared.auth0.com', clientId: 'OZC5JTbnOI1eQJ6-XF0rFFMZqgskh6g6' });
+import Key from './config/AuthKey';
+const auth0 = new Auth0(Key);
 
 auth0
-.webAuth
-.authorize({scope: 'openid email', audience: 'https://kmssquared.auth0.com/userinfo'})
-.then(credentials =>
-  console.log(credentials)
-  // Successfully authenticated
-  // Store the accessToken
-)
-.catch(error => console.log(error));
+  .webAuth
+  .authorize({scope: 'openid email', audience: 'https://kmssquared.auth0.com/userinfo'})
+  .then(credentials =>
+    console.log(credentials)
+    // Successfully authenticated
+    // Store the accessToken
+  )
+  .catch(error => console.log(error));
