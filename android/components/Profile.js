dbcf979ef3e48a1d9151992c49e7ccc8f5f88961
Mark <suyatdesign@gmail.com>
2017-07-31 22:30:30 -0700

    fixed merge conflict

--- android/components/Profile.js
+++ android/components/Profile.js
@@ -5,6 +5,7 @@ import {
   AsyncStorage,
   StyleSheet,
   Text,
+  TextInput,
   View,
   FlatList,
   ScrollView,
@@ -13,6 +14,7 @@ import {
   TouchableHighlight
 } from 'react-native';
 import FAIcons from 'react-native-vector-icons/FontAwesome';
+import Swipeout from 'react-native-swipeout';
 
 let Form = t.form.Form;
 let User = t.struct({
@@ -33,41 +35,87 @@ export default class Profile extends React.Component {
     this.options = {}; // optional rendering options (see documentation)
     // here we are: define your domain model
     this.state = {
-      UserData: {}
+      UserData: {},
+      avoidableList: [],
+      addItem: '',
     }
   }
 
   onPress () {
-    // call getValue() to get the values of the form
-    var value = this.refs.form.getValue();
-    if (value) { // if validation fails, value will be null
-      console.log(value);
-      console.log('body =====', JSON.stringify({
-          user_id: this.state.UserData.user_id,
-          first_name: value.first_name,
-          last_name: value.last_name,
-          avoidables: value.avoidables
-        }));
-      fetch('http://ec2-13-59-228-147.us-east-2.compute.amazonaws.com:8080/edit-profile', {
-        method: 'PUT',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          user_id: this.state.UserData.user_id,
-          first_name: value.first_name,
-          last_name: value.last_name,
-          avoidables: value.avoidables
-        })
-      }).then((response) => {
-        response.json().then((modifiedUser) => {
-          console.log('modified user', modifiedUser);
-          AsyncStorage.setItem('userInfo', JSON.stringify(modifiedUser));
-        }).catch((error) => {console.log('error modifying profile', error)});
-      }).catch((error) => {console.log('error saving profile', error)});
-      this.props.navigation.goBack();
-    }
+    // call getValue() to get the values of the form // if validation fails, value will be null
+    // console.log('body =====', JSON.stringify({
+    //     user_id: this.state.UserData.user_id,
+    //     first_name: this.state.UserData.first_name,
+    //     last_name: this.state.UserData.last_name,
+    //     avoidables: this.state.avoidableList
+    //   }));
+    fetch('http://ec2-13-59-228-147.us-east-2.compute.amazonaws.com:8080/edit-profile', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user_id: this.state.UserData.user_id,
+        first_name: this.state.UserData.first_name,
+        last_name: this.state.UserData.last_name,
+        avoidables: this.state.avoidableList
+      })
+    }).then((response) => {
+      response.json().then((modifiedUser) => {
+        console.log('modified user', modifiedUser);
+        AsyncStorage.setItem('userInfo', JSON.stringify(modifiedUser));
+      }).catch((error) => {console.log('error modifying profile', error)});
+    }).catch((error) => {console.log('error saving profile', error)});
+    this.props.navigation.goBack();
+  }
+
+  deleteItem(index) {
+    this.state.avoidableList.splice(index, 1);
+    this.setState({
+      avoidableList: this.state.avoidableList
+    });
+  }
+
+  addAvoidable() {
+    console.log(this.props)
+    console.log('additem', this.state.addItem)
+    // this.state.avoidableList.push(this.state.addItem);
+    // // console.log('avoidable list after push', this.state.avoidableList)
+    // this.setState({
+    //   avoidableList: this.state.avoidableList
+    // });
+  }
+
+  renderSeparator () {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: Dimensions.get('window').width,
+          backgroundColor: "#CED0CE",
+        }}
+      />
+    );
+  }
+
+  renderItem(item) {
+    let swipeBtns = [{
+      text: 'Delete',
+      backgroundColor: '#F89E3A',
+      onPress: this.deleteItem.bind(this, item.index)
+    }];
+    return (
+      <Swipeout
+        style={styles.swipe}
+        right={swipeBtns}
+        autoClose={true}
+        >
+        <View>
+          <Text style={styles.avoidable}>{item.item.toUpperCase()}</Text>
+        </View>
+      </Swipeout>
+    )
   }
 
 
@@ -86,7 +134,6 @@ export default class Profile extends React.Component {
 
   render () {
     return (
-
       <ScrollView>
         {this.state.UserData.avatar ?
           <View>
@@ -117,19 +164,25 @@ export default class Profile extends React.Component {
           </View>
         }
         <View style={styles.avoidableList}>
-
-          <Text style={{marginBottom: 20}}>Allergens</Text>
+          <Text style={{marginBottom: 10, fontSize: 20, alignSelf: 'center'}}>Allergen List</Text>
           <FlatList
-            data={this.state.UserData.avoidables.split(',')}
-            renderItem={({item}) =>
-
-              <Text style={styles.avoidable}>{item.toUpperCase()}</Text>
-
-            }
-
+            data={this.state.avoidableList}
+            renderItem={this.renderItem.bind(this)}
             ItemSeparatorComponent={this.renderSeparator}
           />
 
+          <View style={{padding: 10, flexDirection: 'row',justifyContent: 'center'}}>
+            <TextInput
+              style={{height: 40, width: "75%", alignSelf: 'center'}}
+              value={this.state.addItem}
+              placeholder="Add other allergens"
+              onChangeText={(addItem) => {this.setState({addItem: addItem})}
+              }
+             />
+            <TouchableHighlight style={{width: 50}}onPress={this.addAvoidable.bind(this)} underlayColor='#99d9f4'>
+            <Text style={{fontSize: 20, alignSelf: 'center', justifyContent:'center'}}>+</Text>
+            </TouchableHighlight>
+          </View>
           <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor='#99d9f4'>
             <Text style={styles.buttonText}>Save</Text>
           </TouchableHighlight>
@@ -155,6 +208,7 @@ const styles = StyleSheet.create({
     right: 0,
     backgroundColor: '#F89E3A',
     padding: 0,
+    height: "100%",
     width: Dimensions.get('window').width,
   },
   overlay: {
@@ -163,12 +217,15 @@ const styles = StyleSheet.create({
   },
   avoidableList: {
     alignSelf: 'center',
-    paddingLeft: 30,
-    paddingRight: 30,
+    paddingLeft: 50,
+    paddingRight: 50,
     paddingTop: 20,
     width: Dimensions.get('window').width,
     backgroundColor: '#ffffff',
     paddingBottom: 20,
+  },
+  swipe: {
+    backgroundColor: '#ffffff'
   },
   title: {
     fontSize: 30,
@@ -195,15 +252,11 @@ const styles = StyleSheet.create({
     fontSize: 12,
     paddingTop: 10,
     paddingBottom: 10,
-    marginLeft: 20,
+    alignSelf: 'center'
   },
   avatarBG: {
     width: Dimensions.get('window').width,
     height: 250,
-    // alignSelf: 'center',
-    // marginBottom: 20,
-    // marginTop: 0,
-    // resizeMode: 'cover',
   },
   avatar: {
     marginTop: 30,
